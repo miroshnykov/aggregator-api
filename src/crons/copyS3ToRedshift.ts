35cff50457676a2aa8c8ce30c17dd24104e83fee
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2021-09-28 11:38:40 -0400

    env REDSHIFT_SCHEMA = advertiser_events     REDSHIFT_TABLE = traffic

--- src/crons/copyS3ToRedshift.ts
+++ src/crons/copyS3ToRedshift.ts
@@ -9,7 +9,7 @@ export const copyS3ToRedshift = async (file:string) => {
 
   let awsKey = process.env.AWS_ACCESS_KEY_ID
   let awsSecretKey = process.env.AWS_SECRET_ACCESS_KEY
-  let dbRedshift = 'advertiser_events.traffic'
+  let dbRedshift = `${process.env.REDSHIFT_SCHEMA}.${process.env.REDSHIFT_TABLE}`
   let bucket = process.env.S3_BUCKET_NAME
   // let file = '/unprocessed/co-offers/2021-09-24/13/20210924132442-292-523.json.gz'
   let queryCopy = `COPY ${dbRedshift} FROM 's3://${bucket}/${destPath}' CREDENTIALS 'aws_access_key_id=${awsKey};aws_secret_access_key=${awsSecretKey}' format as json 'auto' gzip MAXERROR 5 ACCEPTINVCHARS TRUNCATECOLUMNS TRIMBLANKS`;
