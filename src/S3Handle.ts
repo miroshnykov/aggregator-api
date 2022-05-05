1b54faa345f80fd04a500a8676b51d3b8c1fd1c6
dmytro miroshnykov <dmytro.miroshnykov@milkbox.com>
2022-05-05 10:39:12 -0400

    debug stage redshift

--- src/S3Handle.ts
+++ src/S3Handle.ts
@@ -93,7 +93,7 @@ export const copyS3ToRedshift = async (destPath: string) => {
   const dbRedshift = `${process.env.REDSHIFT_SCHEMA}.${process.env.REDSHIFT_TABLE}`;
   const bucket = process.env.S3_BUCKET_NAME;
   const queryCopy = `COPY ${dbRedshift} FROM 's3://${bucket}/${destPath}' CREDENTIALS 'aws_access_key_id=${awsKey};aws_secret_access_key=${awsSecretKey}' format as json 'auto' gzip MAXERROR 5 ACCEPTINVCHARS TRUNCATECOLUMNS TRIMBLANKS`;
-  // consola.info('queryCopy:', queryCopy);
+  consola.info('queryCopy:', queryCopy);
   try {
     await client.query(queryCopy);
     consola.info(`File ${destPath} added to redshift successfully`);
